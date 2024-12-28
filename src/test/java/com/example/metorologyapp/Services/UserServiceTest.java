c76adbb5a9e80eeaf8e676b73707dc8896d7a91e
Max <meitstudying@gmail.com>
2024-12-28 12:14:45 +0200

    #bab-00000 fix: removed unused import

--- src/test/java/com/example/metorologyapp/Services/UserServiceTest.java
+++ src/test/java/com/example/metorologyapp/Services/UserServiceTest.java
@@ -6,7 +6,6 @@ import com.example.metorologyapp.Entities.User;
 import com.example.metorologyapp.Repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.Optional;
 
